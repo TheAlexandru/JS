9274e84ea6ae9e7a4b7ebf842b1472bb6a40dfa4
Artin Alexandru <aaleksandru@gmail.com>
2017-12-19 15:26:12 +0200

    Add files via upload

---
+++ homework/temperature termometer/termometer.js
@@ -0,0 +1,21 @@
+function updateTermometer(){
+	var input = document.getElementById('temperature-input');
+	var div_tempIndic = document.getElementById('termometer-indicator');
+	var alert = 'Error! Insert only numbers between -20 to 50!';
+	var alert_area = document.getElementById('alert_area');
+	var values = input.value;
+	var temp0 = 60; // 0 degree
+	var onegC = 3;//px
+	var showTemp = (onegC * values)+temp0;
+	
+	if(values >= -20 && values <= 50){
+		alert_area.innerText="";
+		div_tempIndic.style.width = `${showTemp}px`;
+	}else{
+		alert_area.innerText= alert;
+	}
+}
+
+var minus = Math.abs(-10);
+var math = 60 + (Math.abs(-10)*3);
+console.log(minus);
