d4491e135c6cecbd92083faf169480236d31fc2d
Artin Alexandru <aaleksandru@gmail.com>
2017-12-19 18:33:03 +0200

    Update game.js

--- homework/dog&bones/game.js
+++ homework/dog&bones/game.js
@@ -1,9 +1,8 @@
 function findNextBone(){
 	var bone = document.getElementsByClassName('bone');
 	if(bone.length > 0){
-		console.log(bone[0]);
 		bone[0].className = 'bone-found';
 	}else{
-			document.getElementById('yard').innerHTML = "GAME OVER";
+		document.getElementById('yard').innerHTML = "GAME OVER";
 		}
 }
