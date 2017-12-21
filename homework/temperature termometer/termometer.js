c569784c5488a4a15a0fb058ebcd9632a93cd3d5
Artin Alexandru <aaleksandru@gmail.com>
2017-12-21 18:24:12 +0200

    Update termometer.js

--- homework/temperature termometer/termometer.js
+++ homework/temperature termometer/termometer.js
@@ -1,17 +1,12 @@
 function updateTermometer(){
-	var input = document.getElementById('temperature-input');
-	var div_tempIndic = document.getElementById('termometer-indicator');
-	var alert = 'Error! Insert only numbers between -20 to 50!';
+	var values = document.getElementById('temperature-input').value;
 	var alert_area = document.getElementById('alert_area');
-	var values = input.value;
-	var temp0 = 60; // 0 degree
-	var onegC = 3;//px
-	var showTemp = (onegC * values)+temp0;
+	var showTemp = (3/*px*/ * values)+60; //60 is default 0 degree
 	
 	if(values >= -20 && values <= 50){
 		alert_area.innerText="";
-		div_tempIndic.style.width = `${showTemp}px`;
+		document.getElementById('termometer-indicator').style.width = `${showTemp}px`;
 	}else{
-		alert_area.innerText= alert;
+		alert_area.innerText= 'Error! Insert only numbers between -20 to 50!';
 	}
 }
