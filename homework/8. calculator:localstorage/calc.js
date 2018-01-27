53adcb17d20762a784ce47e30da2a9077d6d9a0f
Artin Alexandru <aaleksandru@gmail.com>
2018-01-27 13:56:04 +0200

    Add files via upload

---
+++ homework/8. calculator:localstorage/calc.js
@@ -0,0 +1,9 @@
+var inputs = [document.getElementById('firstNr'),document.getElementById('secNr'),document.getElementById('result')]
+function calculate(){
+   result.value = eval(`${inputs[0].value} ${event.target.value} ${inputs[1].value}`); 
+    for(var i=0;i<3;i++){ localStorage.setItem(`Nr_${i}`,inputs[i].value) }
+}
+window.onload= function(){
+    for(var i = 0; i < 4; i++){ document.getElementById('buttons').children[i].addEventListener('click',calculate)}
+    for(var i=0;i<3;i++){ inputs[i].value=localStorage.getItem(`Nr_${i}`) }
+}
