2dfcebf929f7702f06c7786c4a8970f135618a8f
Artin Alexandru <aaleksandru@gmail.com>
2018-01-27 14:00:41 +0200

    Update calc.js

--- homework/8. calculator:localstorage/calc.js
+++ homework/8. calculator:localstorage/calc.js
@@ -1,6 +1,6 @@
 var inputs = [document.getElementById('firstNr'),document.getElementById('secNr'),document.getElementById('result')]
 function calculate(){
-   result.value = eval(`${inputs[0].value} ${event.target.value} ${inputs[1].value}`); 
+   inputs[2].value = eval(`${inputs[0].value} ${event.target.value} ${inputs[1].value}`); 
     for(var i=0;i<3;i++){ localStorage.setItem(`Nr_${i}`,inputs[i].value) }
 }
 window.onload= function(){
